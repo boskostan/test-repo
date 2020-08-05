506cbb3ae90c87495d36a6329761159f21090d74
boskostan <bosko@defensecode.com>
2020-08-05 21:04:17 +0200

    Update Name.aspx.cs

--- Name.aspx.cs
+++ Name.aspx.cs
@@ -14,7 +14,8 @@ namespace demoApplication {
             }
 
             protected void Button1_Click(object sender, EventArgs e) {
-                  Response.Write("Hi " + TextBox1.Text);
+                  string name = TextBox1.Text;
+                  Response.Write("Hi " + name);
             }
       }
 }
