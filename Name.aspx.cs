d0711ec862b57a5e70715aa0168d25fcbe1ead4d
boskostan <bosko@defensecode.com>
2021-09-09 09:45:51 +0200

    Update Name.aspx.cs

--- Name.aspx.cs
+++ Name.aspx.cs
@@ -17,7 +17,7 @@ namespace demoApplication {
             protected void Button1_Click(object sender, EventArgs e) {
                   string name = TextBox1.Text;
                   string contents = File.ReadAllText(name);
-                  Response.Write("Hi to" + name + " , here's your file: \n"+contents);
+                  Response.Write("Hi " + name + " , here's your file: \n"+contents);
             }
       }
 }
